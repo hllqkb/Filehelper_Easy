42d9a752a1b17458d1cca89f7d620276c6fb05d1
hllqkb <androidhtml@yandex.com>
2024-08-29 20:00:31 +0800

    适配了手机

--- index.js
+++ index.js
@@ -197,35 +197,39 @@ function loadAllHistory() {
         });
 }
 function isPhone() {
-    var browser={
-        versions:function(){ 
-               var u = navigator.userAgent, app = navigator.appVersion; 
-               return {//移动终端浏览器版本信息 
-                    trident: u.indexOf('Trident') > -1, //IE内核
-                    presto: u.indexOf('Presto') > -1, //opera内核
-                    webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-                    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-                    mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-                    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-                    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-                    iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
-                    iPad: u.indexOf('iPad') > -1, //是否iPad  
-                    webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
-                    weixin: u.indexOf('MicroMessenger') > -1, //是否微信 
-                    qq: u.match(/\sQQ/i) == " qq" //是否QQ
-                };
-             }(),
-             language:(navigator.browserLanguage || navigator.language).toLowerCase()
-    } 
-     
-      if(browser.versions.mobile || browser.versions.ios || browser.versions.android || 
-          browser.versions.iPhone || browser.versions.iPad){  	
-               return true;
-      }else{
-               return false;
-      }
+    var browser = {
+        versions: function() {
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return { // 移动终端浏览器版本信息
+                trident: u.indexOf('Trident') > -1, // IE内核
+                presto: u.indexOf('Presto') > -1, // opera内核
+                webKit: u.indexOf('AppleWebKit') > -1, // 苹果、谷歌内核
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, // 火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/), // 是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), // ios终端
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, // android终端或者uc浏览器
+                iPhone: u.indexOf('iPhone') > -1, // 是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf('iPad') > -1, // 是否iPad
+                webApp: u.indexOf('Safari') == -1, // 是否web应该程序，没有头部与底部
+                weixin: u.indexOf('MicroMessenger') > -1, // 是否微信
+                qq: u.match(/\sQQ/i) == " qq", // 是否QQ
+                huawei: u.indexOf('Huawei') > -1 || u.indexOf('Honor') > -1, // 是否华为
+                xiaomi: u.indexOf('MI') > -1 || u.indexOf('Redmi') > -1, // 是否小米
+                samsung: u.indexOf('Samsung') > -1, // 是否三星
+                redmi: u.indexOf('Redmi') > -1 // 是否红米
+            };
+        }(),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+    }
 
-      }
+    if (browser.versions.mobile || browser.versions.ios || browser.versions.android ||
+        browser.versions.iPhone || browser.versions.iPad || browser.versions.huawei ||
+        browser.versions.xiaomi || browser.versions.samsung || browser.versions.redmi) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 function Adddeletebutton() {
